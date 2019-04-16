df125c211565af04042dc89d7c09296d6880c91d
StamateValentin <stamatevalentin125@gmail.com>
2019-04-16 17:52:01 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,28 +1,30 @@
-int[] vector;
-int line_w = 4;//this is the line width
-int v_size = 0;
-int sector;
-int i = 0, j = 0;
-void setup(){
-  size(800,400);
-  frameRate(40);
-  background(20);
-  sector = line_w + 2;
-  v_size = width / sector; // a sector must be bigger than the line width in order to have a space betweel lines
-  vector = new int[v_size];
-  for(int i = 0; i < v_size; i++){
-   vector[i] = (int)(random(height-30)); 
-  }
+'use strict';
+let vector = new Array();
+let line_w = 4; //this is the line width
+let v_size = 0;
+let sector;
+let i = 0, j = 0;
+
+function setup(){
+    createCanvas(800,400);
+    background(20);
+    sector = line_w + 2;
+    v_size = width / sector; // a sector must be bigger than the line width in order to have a space betweel lines
+    vector = [v_size];
+    for(let i = 0; i < v_size; i++){ // assign random values
+      vector[i] = parseInt( random( height-30 ) );
+      console.log(vector[i]);
+    }
+
 }
 
-
-void draw(){
-  background(20);
+function draw(){
+    background(20);
   //do the thing "manually"
-  int a = i, b = j,min = vector[a];//finds the minimum number and after swaps it
+  let a = i, b = j,min = vector[a];//finds the minimum number and after swaps it
   if(i<v_size - 1){
 
-     for(int o = a + 1; o<v_size; o++){
+     for(let o = a + 1; o<v_size; o++){
        if(min>vector[o]){
          min = vector[o];
          b = o;
@@ -34,12 +36,11 @@ void draw(){
      j = i;
     
   } else {
-      println("Done");
-      System.out.println("Vector Size: " + v_size);
+      console.log("Vector Size: " + v_size);
       noLoop();
   }
   
-  for(int l = 0; l<v_size; l++){
+  for(let l = 0; l < v_size; l++){
     //all the math behind is having a space between every line
     
     if(l == a){ // highlight the selected one , hope is working
@@ -62,17 +63,10 @@ void draw(){
       
     }
   }
-  
-  
 }
 
-
-void swap(int v[], int i, int j){
- int aux = v[i];
- v[i] = v[j];
- v[j] = aux;
+function swap(v, i, j){
+    let aux = v[i];
+    v[i] = v[j];
+    v[j] = aux;
 }
-//stroke(255);
-//strokeWeight(line_w);
-//all the math behind is having a space between every line
-// line(i*sector + line_w/2, height, i*sector + line_w/2, height - vector[i]);
